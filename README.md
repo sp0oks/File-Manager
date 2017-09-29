74c20e5ab6130f765239ed6bff4b5ecd3718a9c5
Gabriel <g4briel.4lves@gmail.com>
2017-09-29 15:13:46 -0300

    Improved Parser, Readme

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Archiver
+# File Manager
+#### Trabalho 1 da disciplina Organização e Recuperação de Informação
+#### Dupla:
+- Matheus Bortoleto, 726570
+- Gabriel Alves, 726515
+
+## Descrição do arquivo e contexto
+
+O arquivo a ser implementado deve ter as seguintes características:
+
+   * Não estabelecer qualquer ordenação entre seus registros;
+   * Possuir registros de tamanho fixo e no máximo 100 bytes cada um;
+   * Ter controle de blocos, de forma que cada bloco possua um número inteiro de registros.
+
+Para esta implementação, supõe-se que os blocos possuem 512 bytes cada um.
+
+## Proposta do trabalho
+
+O trabalho consiste na implementação de um programa que permita as seguintes operações:
+
+   * Criação de um arquivo vazio;
+   * Inserção de um novo registro;
+   * Busca por um registro, dada uma chave;
+   * Remoção de um registro, dada uma chave;
+   * Listagem dos registros;
+   * Compactação do arquivo.
+
+O arquivo de dados pode ser fixo no programa. Ou seja, não é preciso que o usuário indique o seu nome.
+
+O formato e conteúdo dos registros é livre, obedecida a limitação do tamanho máximo já definido.
+
+A operação de **criação** consiste em criar um arquivo vazio. Esta operação pode sobrescrever um arquivo preexistente.
+
+A **inserção** de um registro deve fazer a leitura dos dados e inserir o registro no arquivo. Devem ser aproveitados os espaços disponíveis gerados por remoções. Não deve haver preocupação quanto a repetições de valores. Opcionalmente à leitura, os dados podem ser gerados aleatoriamente.
+
+Uma **busca** por um registro deve ser feita dado o valor de uma chave. Cada trabalho deve eleger uma única chave para esta busca. Como há possibilidade de repetições, apenas a primeira ocorrência da chave deve ser retornada. O conteúdo do registro encontrado deve ser apresentado no caso de busca bem sucedida.
+
+A **remoção**, que é precedida de uma busca, deve ser feita de forma lógica. Assim, apenas deve-se controlar a validade ou não dos registros.
+
+A **listagem** corresponde à apresentação de todos os registros válidos existentes no arquivo, na ordem física em que aparecem.
+
+A operação de **manutenção** corresponde à compactação dos dados do arquivo, ou seja, à eliminação de todos os espaços disponíveis. Esta operação deve eliminar também blocos que fiquem vazios.
+
+Uma opção para **inserção "em lote"** pode ser implementada e é bem-vinda. Nesta opção, solicita-se quantos registros devem ser inseridos, chamando-se a operação de inserção sucessivamente com dados aleatórios.
+
+A implementação pode ser feita nas seguintes linguagens de programação:
+
+   * C
+   * C++
+   * Java
+   * Python
+
+## O que enviar e quando
+
+Deve ser enviado um único arquivo com o código elaborado. Caso seja feita a opção pela codificação em vários arquivos (e.g., main.c, registro.h, arquivo.h etc.), os vários arquivos devem ser enviados dentro de um arquivo zip, rar ou tgz. Formatos de arquivos compactados que não puderem ser abertos pelo professor serão desconsiderados.
+
+Apenas um dos membros do grupo deve enviar o trabalho.
+
+O envio será aceito apenas até a data limite estabelecida. Atrasos serão bloqueados e, portanto, desconsiderados.
+
+## Critérios de avaliação
+
+A avaliação do trabalho se dará mediante análise e execução do código enviado.
+
+A pontuação contemplará:
+
+   * Documentação do programa e do código (até 1,0 ponto)
+   * Uso de boas práticas de codificação, como identificadores significativos, indentação, codificação eficiente etc. (até 1,0 ponto)
+   * Uso correto da manipulação de arquivos, tanto pela codificação quanto pela eficiência (até 2,0 pontos)
+   * Implementação da busca (até 1,0 ponto)
+   * Implementação da inserção e remoção (3,0 pontos)
+   * Implementação da listagem (1,0 ponto)
+   * Implementação da compactação (1,0 ponto)
+
+A aderência à proposta é essencial. Por exemplo, os trabalhos que não fizerem controle de blocos não serão considerados, mesmo que todas as operações estejam corretamente implementadas; e as partes de inserção e remoção serão desconsideradas se não houver gerenciamento dos espaços disponíveis.
--- src/main.c
+++ src/main.c
@@ -1,6 +1,4 @@
-/*
-
-*/
+/**/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,25 +11,26 @@ int readflags(int*, char**, char**, char**, char**, int, char**);
 
 int main(int argc, char* argv[]){
     // Getopt related (flags, file path, iterators, etc.)
-    int flags[5] = {0, 0, 0, 0, 0}; // [n, s, r, i, c]
+    int flags[6] = {0, 0, 0, 0, 0, 0}; // [n, s, r, i, c, l]
     char *sarg = NULL; // Search argument
     char *rarg = NULL; // Remove argument
     char *iarg = NULL; // Insert argument
-    char *filepath = NULL; // File path argument   
+    char *filepath = NULL; // File path argument
 
     // Insertion related
     unsigned int regnum; // Number of register to insert
     reg regin; // Array of registers to insert
 
     int index;
-
-    // Checks if there's a file path argument
-    // If file path argument is missing, print the usage and exit with 1
+    int exitcode = 0;
     /*
-        Options order:
-        n > s > r > i > c
-        
-        So, if all the options are selected, the program will first create the file, then search it, remove the register, insert new registers, and then compact the file.
+     Checks if there's a file path argument
+     If file path argument is missing, print the usage and exit with 1
+
+     Options order:
+     n > s > r > i > c > l
+
+     So, if all the options are selected, the program will first create the file, then search it, remove the register, insert new registers, compact the file and finally list current registers in the file.
     */
     if(argc < 2){
         printhelp();
@@ -39,106 +38,106 @@ int main(int argc, char* argv[]){
     }
 
     if(!readflags(flags, &sarg, &rarg, &iarg, &filepath, argc, argv))
-        return 1;
+        exitcode = 1;
 
-    // Now for the operations, it's necessary to mantain the precedence
-    // 0. If no option flag is set, only list register
-    if(flags[0] != 0 && flags[1] != 0 && flags[2] != 0 && flags[3] != 0 && flags[4] != 0){
-        listreg(filepath); 
-
-        // Free all argument strings       
-        free(sarg);
-        free(rarg);
-        free(iarg);
-        free(filepath);
-
-        return 0;
-    }
+    // Now for the operations, it's necessary to maintain the precedence
 
     // 1. Create a new file
     if(flags[0] == 1){
         // If it's not possible to create the file, quits the program
         if(!createfile(filepath)){
             printf("Failed to create file %s\n", filepath);
-            return 1;
+            exitcode = 1;
         }
-        printf("Created file successfully\n");
+        else
+            printf("Created file successfully\n");
     }
-    
+
     // 2. Search register
     if(flags[1] == 1){
         if(!searchreg(&regin, sarg, filepath)){
             printf("Search on %s failed\n", filepath);
-            return 1;
+            exitcode = 1;
         }
-        
-        printf("%s %s %s %s", regin.key, regin.title, regin.author, regin.link);
-        
-        free(sarg);
+        else
+            printf("[ %s | %s | %s | %s ]", regin.key, regin.title, regin.author, regin.link);
     }
-    
+
     // 3. Remove register
-    if(flags[2] == 1){
+    if(flags[2] == 1)
         removereg(rarg, filepath);
-        free(rarg);
-    }
 
     // 4. Insert registers
     if(flags[3] == 1){
-        regnum = (iarg == NULL)?1:(atoi(iarg)==0)?1:atoi(iarg);
+        regnum = (iarg == NULL) ? 1 : (atoi(iarg) == 0) ? 1 : atoi(iarg);
         for(index = 0; index < regnum; index++){
             readreg(&regin);
             insertreg(regin, filepath);
         }
-        free(iarg);
     }
-    
-    // 5. Compact file
-    if(flags[4] == 1){
-        compactfile(filepath);
-        free(filepath);
-    }   
 
-    return 0;
+    // 5. Compact file
+    if(flags[4] == 1)
+        compactfile(filepath);
+
+    // 6. List registers
+    if(flags[5] == 1)
+        listreg(filepath);
+
+    // Free all the argument strings that were allocated
+    if(sarg)
+      free(sarg);
+    if(rarg)
+      free(rarg);
+    if(iarg)
+      free(iarg);
+    if(filepath)
+      free(filepath);
+
+    return exitcode;
 }
 
 void printhelp(){
-    printf("usage: archiver [options] file_path\n");
+    printf("\tusage: archiver [options] <file_path>\n");
     printf("\toptions:\n");
     printf("\t\t-n\t\t\tCreates a new file\n");
-    printf("\t\t-i[number_registers]\tInserts registers into file\n");
-    printf("\t\t-s register_key\t\tSearch register into file\n");
-    printf("\t\t-r register_key\t\tRemoves register\n");
+    printf("\t\t-i <reg_number>\t\tInserts <reg_number> registers into file\n");
+    printf("\t\t-s <reg_key>\t\tSearches register key in file\n");
+    printf("\t\t-r <reg_key>\t\tRemoves register from file\n");
     printf("\t\t-c\t\t\tCompact file\n");
+    printf("\t\t-l\t\t\tList all registers in file\n");
 }
 
 int readflags(int* flags, char** sarg, char** rarg, char** iarg, char** filepath, int argc, char** argv){
-    int opt;    
+    int opt;
 
     // Reads all the options from command line
-    // The options set flags to 1, an if a argument is needed, it's passed to a char*
-    while((opt = getopt(argc, argv, "ncr:s:i::")) != -1){
+    // The options set flags to 1, and if an argument is needed, it's passed to a char pointers
+    while((opt = getopt(argc, argv, "nclr:s:i::")) != -1){
         switch(opt){
             case 'n':
                 flags[0] = 1;
-                break;
-            case 'c':
-                flags[4] = 1;
-                break;
-            case 'r':
-                flags[2] = 1;
-                *rarg = malloc(strlen(optarg));
-                strcpy(*rarg, optarg);
                 break;
             case 's':
                 flags[1] = 1;
                 *sarg = malloc(strlen(optarg));
                 strcpy(*sarg, optarg);
                 break;
+            case 'r':
+                flags[2] = 1;
+                *rarg = malloc(strlen(optarg));
+                strcpy(*rarg, optarg);
+                break;
             case 'i':
                 flags[3] = 1;
                 *iarg = malloc(strlen(optarg));
                 strcpy(*iarg, optarg);
+                break;
+            case 'c':
+                flags[4] = 1;
+                break;
+            case 'l':
+                flags[5] = 1;
                 break;
         }
     }
